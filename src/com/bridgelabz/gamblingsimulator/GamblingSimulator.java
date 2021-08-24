b2cb088028f67facd6af30e2c5398dca82b35680
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-24 09:05:19 +0530

    ADDED : gambling game

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -4,23 +4,23 @@ public class GamblingSimulator {
 	static final int TOTALSTAKE = 100;
 	static final int BET = 1;	
 	
-	private static int GamblingGame(int currentBalance) {
+	private static int GamblingGame(int currentStake) {
 		int i = (int) Math.floor(Math.random()*10)%2;
 		if(i == 0) {
 			System.out.println("Gambler has lost the game");
-			return currentBalance-1;
+			return currentStake-1;
 		}
 		else {
 			System.out.println("Gambler has won the game");
-			return currentBalance+1;
+			return currentStake+1;
 		}
 	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Starting with stake of $100 and betting $1 every game");
-		int currentBalance = TOTALSTAKE;
-		currentBalance = GamblingGame(currentBalance);
+		int currentStake = TOTALSTAKE;
+		currentStake = GamblingGame(currentStake);
 		
 		
 	}
