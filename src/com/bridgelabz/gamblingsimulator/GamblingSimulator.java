2a8ae716bbd7adbc5453d612287f9fe51bbc6d27
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-24 19:25:57 +0530

    ADDED: monthly report generator

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,19 +3,37 @@ package com.bridgelabz.gamblingsimulator;
 public class GamblingSimulator {
 	static final int TOTALSTAKE = 100;
 	static final int BET = 1;	
+	static final int TOTALDAYS = 30;
+	static final int TOTALMONTHS = 12;
 	static int stake = TOTALSTAKE;
-	static final int TOTALDAYS = 20;
+	
 	static int finalAmount  = 0;
 	static int amountLost = 0;
 	static int amountGained =0;
-	private static int TotalAmount() {
+	static int daysWon = 0;
+	static int daysLost = 0;
+	
+	
+	private static void monthlyReport() {
+			amountPerMonth();
+			System.out.println("Monthy Report :");
+			System.out.println("Final Amount : " + finalAmount);
+			System.out.println("Number of days won : "+daysWon);
+			System.out.println("Number of days lost : "+daysLost);
+			System.out.println("Amount lost : "+ amountLost+" Amount Gained : "+ amountGained);
+			System.out.println();
+			finalAmount = 0; amountLost =0; amountGained = 0; daysLost = 0; daysWon = 0; 
+		
+	}
+	
+	private static void amountPerMonth() {
 		int days = 0;
 		while(days < TOTALDAYS) {
 		stake = TOTALSTAKE;
 		finalAmount += playGame(stake);
 		days++;
 		}
-		return finalAmount;
+		
 	}
 	
 	
@@ -25,19 +43,18 @@ public class GamblingSimulator {
 		while(currentStake > minLimit && currentStake < maxLimit) {
 			currentStake = GamblingGame(currentStake);
 		}
+		if(currentStake == minLimit) daysLost++;
+		else daysWon++;
 		return currentStake;
 	}
 	
 	private static int GamblingGame(int currentStake) {
 		int i = (int) Math.floor(Math.random()*10)%2;
 		if(i == 0) {
-			System.out.println("Gambler has lost the game");
 			amountLost += 1;
 			return currentStake-1;
-			
 		}
 		else {
-			System.out.println("Gambler has won the game");
 			amountGained += 1;
 			return currentStake+1;
 		}
@@ -46,10 +63,9 @@ public class GamblingSimulator {
 	public static void main(String[] args) {
 		
 		System.out.println("Starting with stake of $100 and betting $1 every game");
+			
+		monthlyReport();
 		
-		finalAmount = TotalAmount();
-		System.out.println("Final Amount" + finalAmount);
-		System.out.println("Amount lost : "+ amountLost+" Amount Gained : "+ amountGained);
 	}
 
 }
