864b71b7d844a52eb253248bcc9f34cfbc8c8c67
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-24 08:21:30 +0530

    Initial upload

---
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		int totalStake = 100;
+		int stakePerGame = 1;
+		System.out.println("Starting with stake of $100 and betting $1 every game");
+
+	}
+
+}
